8405900b410277f6a42d1c3cac99bc8ed55f7eab
ash24ish <ash24ish@gmail.com>
2021-05-03 03:03:52 +0530

    This branch shows how to use the ServiceConnection class to connect to Service.  Bound Service, Unbound Service, use of Ibinder, ServiceConnection

--- app/src/main/java/com/ashishbharam/servicedemo/MainActivity.java
+++ app/src/main/java/com/ashishbharam/servicedemo/MainActivity.java
@@ -3,30 +3,42 @@ package com.ashishbharam.servicedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     MyService myService;
-    Button btnStartMyService,btnStopMyService;
-
+    Button btnStartMyService, btnStopMyService, btnUnbind, btnBind, btnShowNumber;
+    TextView tvShowNumber;
     private Intent serviceIntent;
+
+    private boolean isServiceBound;
+    private ServiceConnection serviceConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("TAG", "if MainActivity thread ID is same as service: "+Thread.currentThread().getId());
+        Log.i("TAG", "if MainActivity thread ID is same as service: " + Thread.currentThread().getId());
         Log.i("TAG", "that means MainActivity & MyService is running on same thread ");
 
-        btnStartMyService =findViewById(R.id.btnStartService);
-        btnStopMyService =findViewById(R.id.btnStopService);
+        btnStartMyService = findViewById(R.id.btnStartService);
+        btnStopMyService = findViewById(R.id.btnStopService);
+        btnBind = findViewById(R.id.btnBindService);
+        btnUnbind = findViewById(R.id.btnUnbindService);
+        btnShowNumber = findViewById(R.id.btnShowNumber);
+        tvShowNumber = findViewById(R.id.tvRandomNum);
 
-        serviceIntent = new Intent(getApplicationContext(),MyService.class);
+        serviceIntent = new Intent(getApplicationContext(), MyService.class);
 
         btnStartMyService.setOnClickListener(v -> {
             Log.i("TAG", "Service Started: ");
@@ -38,5 +50,38 @@ public class MainActivity extends AppCompatActivity {
             stopService(serviceIntent);
         });
 
+        btnBind.setOnClickListener(v -> {
+            if (serviceConnection == null) {
+                serviceConnection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName name, IBinder iBinder) {
+                        MyService.MyServiceBinder myServiceBinder = (MyService.MyServiceBinder) iBinder;
+                        myService = myServiceBinder.getService();
+                        isServiceBound = true;
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName name) {
+                        isServiceBound = false;
+                    }
+                };
+            }
+            bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        });
+
+        btnUnbind.setOnClickListener(v -> {
+            if (isServiceBound) {
+                unbindService(serviceConnection);
+                isServiceBound = false;
+            }
+        });
+
+        btnShowNumber.setOnClickListener(v -> {
+            if (isServiceBound) {
+                tvShowNumber.setText("" + myService.getRandomNumber());
+            } else {
+                tvShowNumber.setText("Service not Bound");
+            }
+        });
     }
 }
--- app/src/main/java/com/ashishbharam/servicedemo/MyService.java
+++ app/src/main/java/com/ashishbharam/servicedemo/MyService.java
@@ -3,21 +3,83 @@ package com.ashishbharam.servicedemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class MyService extends Service {
+    private int mRandomNumber;
+    private boolean isRandomGeneratorOn;
+
+    class MyServiceBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    private IBinder iBinder = new MyServiceBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.i("TAG", "In onBind(): Executes only once, Components can be bind to both started,stopped or unstarted service");
+        return iBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i("TAG", "In onUnbind(): Executes only once, you can not stop service if don't Unbind it.");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("TAG", "Service Destroyed : if you call to stop a service before unbinding it, " +
+                "it will stop automatically after all components from the service are unbound ");
+        stopRandomNumberGenerator();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("TAG", " MyService thread ID: "+Thread.currentThread().getId());
-        return super.onStartCommand(intent, flags, startId);
+        Log.i("TAG", "In onStartCommand, MyService thread ID: " + Thread.currentThread().getId());
+        isRandomGeneratorOn = true;
+        //here we are running function on separate thread bcz service->onStartCommand() runs on main thread.
+        //if we don't use separate thread it will block the UI and lead to ANR.
+        new Thread(() -> {
+            startRandomNumberGenerator();
+        }).start();
+
+        //return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    private void startRandomNumberGenerator() {
+        while (isRandomGeneratorOn) {
+            try {
+                Thread.sleep(1000);
+                if (isRandomGeneratorOn) {
+                    mRandomNumber = new Random().nextInt(999 - 99) + 99;
+                    Log.i("TAG", "startRandomNumberGenerator Thread id: " + Thread.currentThread().getId()
+                            + " Random num:" + mRandomNumber);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.d("TAG", "Thread Interrupted :" + e.getLocalizedMessage());
+
+            }
+        }
+    }
+
+    public void stopRandomNumberGenerator() {
+        isRandomGeneratorOn = false;
+    }
+
+    public int getRandomNumber() {
+        return mRandomNumber;
     }
 }
